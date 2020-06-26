d6f04ce522a4a546b39e2f0a0e7803c19c35a106
Biihuy <biihuy25697@gmail.com>
2020-06-26 17:30:21 +0700

    fix 1

--- js/js.js
+++ js/js.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($) {
 	var showToTop = -1;
 	window.onscroll = function(){
 		curPos = window.pageYOffset;
-		console.log(curPos);
+
 		var menu = $('.head-menu');
 		if(prevPos < curPos)
 		{
@@ -88,38 +88,86 @@ jQuery(document).ready(function($) {
 			$('.menu-item').removeClass('show');
 		}
 	});
+
 	var _index = 1;
 	var slideItem = $('.slide--item');
-	var slideRecall;
-	recallSlide();
-	function recallSlide() {
-	    for (var i = 1; i <= slideItem.length; i++) {
-	        if($('.slide--item:nth-child('+i+')').hasClass('active'))
-	        	{
-	        		$('.slide--item:nth-child('+i+')').addClass('slideOutLeft').delay(delayTime*.5).queue(function(next){
-						$(this).removeClass('active').removeClass('slideOutLeft');
-						var _i = ++_index;
-	        			if(_i > slideItem.length)
-	        			{
-	        				_index = 1;
-	        				_i = 1;
-	        			}
-		        		if(_i <= slideItem.length){
-		        			$('.slide--item:nth-child('+_index+')').addClass('active').addClass('slideInRight').delay(delayTime).queue(function(next){
-		        				$(this).removeClass('slideInRight');
-		        				next();
-		        			});
-		        		}
-		        			next();
-		        	});
-	        	break;
-	        }
-	    }
-	    slideRecall = setTimeout(recallSlide, 4000);
-	}
-	$('.to-top').click(function(event) {
-		$('html, body').animate({
-          scrollTop: 0
-        }, "slow");
+	var _slideRecall;
+	
+	var buttonLeft = $('.slide--btn-left');
+	var buttonRight = $('.slide--btn-right');
+
+
+  	$('.sliding').slick({
+  		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
 	});
+	var _logoSlideItemCount = 5;
+	if($(window).width()<764) {
+		_logoSlideItemCount = 2;
+	}
+  	$('.slide--logo').slick({
+		//slidesToShow: 
+		slidesToShow: _logoSlideItemCount,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
+	// recallSlide(_index);
+	// function recallSlide(ind) {
+	// 	if(ind > slideItem.length){
+	// 		_index = 1;
+	// 	}
+	// 	if(ind < slideItem.length){
+	// 		_index = slideItem.length;
+	// 	}
+	//     // for (var i = 1; i <= slideItem.length; i++) { 
+	//     //     if($('.slide--item:nth-child('+i+')').hasClass('active'))
+	//     //     	{
+	//     //     		$('.slide--item:nth-child('+i+')').addClass('slideOutLeft').delay(delayTime*.5).queue(function(next){
+	// 				// 	$(this).removeClass('active').removeClass('slideOutLeft');
+	// 				// 	var _i = ++_index;
+	//     //     			if(_i > slideItem.length)
+	//     //     			{
+	//     //     				_index = 1;
+	//     //     				_i = 1;
+	//     //     			}
+	// 	   //      		if(_i <= slideItem.length){
+	// 	   //      			$('.slide--item:nth-child('+_index+')').addClass('active').addClass('slideInRight').delay(delayTime).queue(function(next){
+	// 	   //      				$(this).removeClass('slideInRight');
+	// 	   //      				next();
+	// 	   //      			});
+	// 	   //      		}
+	// 	   //      			next();
+	// 	   //      	});
+	//     //     	break;
+	//     //     }
+	//     // }
+
+	//     for(var i = 1; i <= slideItem.length; i++)
+	//     {	
+	//     	if(i == _index){
+	//     		$('.slide--item:nth-child('+i+')').addClass('slideOutLeft').delay(delayTime*.5).queue(function(next){
+	// 					$(this).removeClass('active').removeClass('slideOutLeft');
+	// 					var _i = ++_index;
+	//         			if(_i > slideItem.length)
+	//         			{
+	//         				_index = 1;
+	//         				_i = 1;
+	//         			}
+	// 	        		if(_i <= slideItem.length){
+	// 	        			$('.slide--item:nth-child('+_index+')').addClass('active').addClass('slideInRight').delay(delayTime).queue(function(next){
+	// 	        				$(this).removeClass('slideInRight');
+	// 	        				next();
+	// 	        			});
+	// 	        		}
+	// 	        		next();
+	// 	        	});
+	//         	break;
+	//     	}
+
+	//     }
+	//     slideRecall = setTimeout(recallSlide, 4000);
+	// }
 });
